3c333a56bc1f8afc67b2402a8d53161d1a15bc23
lastriondigital <lastriondigital@gmail.com>
2026-09-25 22:46:09 +0000

    feat: add Android APK build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lastriondigital.leadion',
+  appName: 'leadion',
+  webDir: 'dist'
+};
+
+export default config;
